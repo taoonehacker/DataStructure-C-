879441b5a30f13f8e30b48a690b5ab1422757b6c
taoonehacker@gmail.com <taoonehacker@gmail.com>
2019-10-09 22:44:19 +0800

    Add getSize getCapacity and Add Method to Array

--- TaoOnehacker.DataStructure.Application/Array.cs
+++ TaoOnehacker.DataStructure.Application/Array.cs
@@ -4,6 +4,58 @@ namespace TaoOnehacker.DataStructure.Application
 {
     public class Array
     {
+        private int[] data;
+        private int size;
+
+        public Array(int capacity)
+        {
+            data = new int[capacity];
+            size = 0;
+        }
+
+        public Array():this(10)
+        {
+        }
+
+        public int GetCapacity()
+        {
+            return data.Length;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public void Add(int index, int e)
+        {
+            if(index==data.Length)
+                throw new ArgumentException("Add failed. Array is full.");
+            
+            if(index<0||index>size)
+                throw  new ArgumentException("Add failed. Required index >=0 and index<= size.");
+
+            for (var i = size-1; i >= index; i--)
+            {
+                data[i + 1] = data[i];
+            }
+
+            data[index] = e;
+            
+            size++;
+        }
+
+        public void AddFirst(int e)
+        {
+            Add(0,e);
+        }
+
+        public void AddLast(int e)
+        {
+            Add(size,e);
+        }
+        
+        
         
     }
 }
--- TaoOnehacker.DataStructure.Test/ArrayTest.cs
+++ TaoOnehacker.DataStructure.Test/ArrayTest.cs
@@ -1,18 +1,50 @@
 using NUnit.Framework;
+using TaoOnehacker.DataStructure.Application;
 
 namespace TaoOnehacker.DataStructure.Test
 {
     public class ArrayTest
     {
+        private Array _array;
         [SetUp]
         public void Setup()
         {
+            _array = new Array(20);
         }
 
         [Test]
-        public void Test1()
+        public void GetCapacityTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(20, _array.GetCapacity());
         }
+
+        [Test]
+        public void GetSizeTest()
+        {
+            Assert.AreEqual(0,_array.GetSize());
+        }
+        [Test]
+        public void AddTest()
+        {
+            _array.Add(0, 1);
+            Assert.AreEqual(1,_array.GetSize());
+        }
+
+        [Test]
+        public void AddFirstTest()
+        {
+            _array.AddFirst(1);
+            Assert.AreEqual(1,_array.GetSize());
+        }
+        
+        [Test]
+        public void AddLastTest()
+        {
+            _array.AddLast(1);
+            Assert.AreEqual(1,_array.GetSize());
+        }
+        
+        
+        
     }
 }
