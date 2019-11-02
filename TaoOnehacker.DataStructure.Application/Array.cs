3f0695637db9866a05b51194377add130fa69797
taoonehacker@gmail.com <taoonehacker@gmail.com>
2019-11-02 11:09:57 +0800

    Add Find Set and Remove Method

--- TaoOnehacker.DataStructure.Application/Array.cs
+++ TaoOnehacker.DataStructure.Application/Array.cs
@@ -4,45 +4,41 @@ namespace TaoOnehacker.DataStructure.Application
 {
     public class Array
     {
-        private int[] data;
-        private int size;
+        private readonly int[] _data;
+        private int _size;
 
         public Array(int capacity)
         {
-            data = new int[capacity];
-            size = 0;
+            _data = new int[capacity];
+            _size = 0;
         }
 
-        public Array():this(10)
-        {
-        }
+        public Array():this(10){}
 
         public int GetCapacity()
         {
-            return data.Length;
+            return _data.Length;
         }
 
         public int GetSize()
         {
-            return size;
+            return _size;
         }
 
         public void Add(int index, int e)
         {
-            if(index==data.Length)
+            if(index==_data.Length)
                 throw new ArgumentException("Add failed. Array is full.");
             
-            if(index<0||index>size)
-                throw  new ArgumentException("Add failed. Required index >=0 and index<= size.");
+            if(index<0||index>_size)
+                throw  new ArgumentException("Add failed. Required Index >=0 and Index<= size.");
 
-            for (var i = size-1; i >= index; i--)
-            {
-                data[i + 1] = data[i];
+            for (var i = _size-1; i >= index; i--){
+                _data[i + 1] = _data[i];
             }
-
-            data[index] = e;
             
-            size++;
+            _data[index] = e;
+            _size++;
         }
 
         public void AddFirst(int e)
@@ -52,10 +48,69 @@ namespace TaoOnehacker.DataStructure.Application
 
         public void AddLast(int e)
         {
-            Add(size,e);
+            Add(_size,e);
         }
-        
-        
-        
+
+        public int Get(int index)
+        {
+            if(index<0||index>_size)
+                throw new ArgumentException("Get failed. Index is illegal.");
+            return _data[index];
+        }
+
+        public void Set(int index, int e)
+        {
+            if(index<0||index>_size)
+                throw new ArgumentException("Get failed.Index is illegal.");
+            _data[index] = e;
+        }
+
+        public bool Contains(int e)
+        {
+            for (var i = 0; i < _size; i++)
+                return _data[i] == e;
+
+            return false;
+        }
+
+        public int Find(int e)
+        {
+            for (var i = 0; i < _size; i++)
+                if (_data[i] == e)
+                    return i;
+            return -1;
+        }
+
+        public int Remove(int index)
+        {
+            if(index<0||index>_size)
+                throw new ArgumentException("Get failed.Index is illegal.");
+
+            var res = _data[index];
+            for(var i=index+1;i<_size;i++)
+            {
+                _data[i-1] = _data[i];
+            }
+            _size--;
+            return res;
+        }
+
+        public int RemoveFirst()
+        {
+            return Remove(0);
+        }
+
+        public int RemoveLast()
+        {
+            return Remove(_size-1);
+        }
+
+
+        public void RemoveElement(int e)
+        {
+            var index = Find(e);
+            if (index > 0)
+                Remove(index);
+        }
     }
 }
--- TaoOnehacker.DataStructure.Test/ArrayTest.cs
+++ TaoOnehacker.DataStructure.Test/ArrayTest.cs
@@ -6,6 +6,7 @@ namespace TaoOnehacker.DataStructure.Test
     public class ArrayTest
     {
         private Array _array;
+        
         [SetUp]
         public void Setup()
         {
@@ -42,6 +43,61 @@ namespace TaoOnehacker.DataStructure.Test
         {
             _array.AddLast(1);
             Assert.AreEqual(1,_array.GetSize());
+        }
+
+        [Test]
+        public void GetTest()
+        {
+            for (var i = 0; i < 10; i++){
+                _array.AddLast(i);
+            }
+            
+            Assert.AreEqual(5,_array.Get(5));
+        }
+
+        [Test]
+        public void SetTest()
+        {
+            for (var i = 0; i < 10; i++){
+                _array.AddLast(i);
+            }
+
+            _array.Set(1, 20);
+            
+            Assert.AreEqual(20,_array.Get(1));
+        }
+
+        [Test]
+        public void Contains()
+        {
+            for (var i = 0; i < 10; i++){
+                _array.AddLast(i);
+            }
+            
+            Assert.IsTrue(_array.Contains(9));
+        }
+
+        [Test]
+        public void Find()
+        {
+            for (var i = 0; i < 10; i++){
+                _array.AddLast(i);
+            }
+            Assert.AreEqual(5, _array.Find(5));
+            Assert.AreEqual(-1, _array.Find(20));
+        }
+
+        [Test]
+        public void Remove()
+        {
+            for (var i = 0; i < 10; i++){
+                _array.AddLast(i);
+            }
+
+            _array.Remove(5);
+            
+            Assert.AreEqual(6, _array.Get(5));
+
         }
         
         
